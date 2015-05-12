326791bd736b7ae44e49cc7a8699507368a79697
yoneshigeian <yoneshigeian@gmail.com>
2015-05-12 13:58:48 -1000

    Shooter State, ShooterSpeed, and SetShooter FINSHED BUT NOT TESTED

--- shooter.h
+++ shooter.h
@@ -1,5 +1,5 @@
 //Edited 1:20pm 5/12
-void ShooterStateMachine()
+void ShooterStateMachine() //Counter keeps track of buttons pushed to increase state
 {
   if(shootstate >=0 && shootstate <= 99)
   {
@@ -21,4 +21,52 @@ void ShooterStateMachine()
     if(SHOOTDOWN==NOTPRESS)
     {shootstate=shootstate+200}
   }
+}
+
+void ShooterSpeed(int speed)//uses motors shootfrontleft, shootbackleft, shootfrontright, shootbackright
+{
+  motor[shootfrontleft]=  speed;
+  motor[shootbackleft]=   speed;
+  motor[shootfrontright]= speed;
+  motor[shootbackright]=  speed;
+}
+
+void SetShooter() //sets shooter speed based on shooter state
+{
+  if(shootstate==0)
+  {ShooterSpeed(0);}
+  
+  if(shootstate=1)
+  {ShooterSpeed(30);}
+  
+  if(shooterstate==2)
+  {ShooterSpeed(60);}
+   
+  if(shooterstate==3)
+  {ShooterSpeed(90);}
+  
+  if(shooterstate==4)
+  {ShooterSpeed(95);} 
+  
+  if(shooterstate==5)
+  {ShooterSpeed(100);} 
+  
+  if(shooterstate==6)
+  {ShooterSpeed(105);} 
+  
+  if(shooterstate==7)
+  {ShooterSpeed(110);} 
+  
+  if(shooterstate==8)
+  {ShooterSpeed(115);} 
+  if(shooterstate==9)
+  {ShooterSpeed(120);} 
+  if(shooterstate==10)
+  {ShooterSpeed(125);} 
+  if(shooterstate==11)
+  {ShooterSpeed(127);} 
+  
+  
+  
+  
 }
